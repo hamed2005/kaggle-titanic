ee353e7ef31a6f8afb97afef34cda3b33eb3b347
hamed <hamed2005@gmail.com>
2022-05-18 10:32:39 +0200

    Initialize cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle titanic dataset competition',
+    author='Hamed',
+    license='MIT',
+)
